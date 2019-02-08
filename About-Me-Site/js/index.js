579943f89601649649d9e8183a3845f2b2bf3fc6
lfonzi62 <lfontenette@berkeley.edu>
2019-02-08 14:36:57 -0800

    some responsiveness

--- About-Me-Site/js/index.js
+++ About-Me-Site/js/index.js
@@ -12,6 +12,26 @@ console.log(`${love}`);
 
 
 
+
+
+// Quote function that times
+(function() {
+
+    var quotes = $(".quotes");
+    var quoteIndex = -1;
+
+    function showNextQuote() {
+        ++quoteIndex;
+        quotes.eq(quoteIndex % quotes.length)
+            .fadeIn(2000)
+            .delay(2000)
+            .fadeOut(2000, showNextQuote);
+    }
+
+    showNextQuote();
+
+})();
+
 // lightbox function
 $(document).on('click', '[data-toggle="lightbox"]', function(event) {
                 event.preventDefault();
